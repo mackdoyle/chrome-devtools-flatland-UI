e264f5bcc4121031c84b79492b4e12da5dd1e41f
Chase Adams <chase@realchaseadams.com>
2013-12-22 21:07:43 -0800

    Sassified and grunt watch tasks and some additional styling for network and search in console panes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,25 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['sass/*.sass'],
+        tasks: ['sass']
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'styles.css': 'sass/flatland.sass'
+        }
+      }
+    }
+  });
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['watch']);
 };
