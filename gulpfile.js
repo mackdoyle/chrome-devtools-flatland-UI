df3537bcd8817c79fd864e863d4e1b3afee87616
Chase Adams <chase@realchaseadams.com>
2014-03-05 20:04:55 -0800

    Add run sequence for bumping pre compression without async bugs

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,18 @@ var lr = require('gulp-livereload');
 var sass = require('gulp-sass');
 var bump = require('gulp-bump');
 var zip = require('gulp-zip');
+var runSequence = require('run-sequence');
 
 gulp.task('default', ['watch']);
-gulp.task('pubmajor', ['bump', 'compress']);
-gulp.task('pubminor', ['bump', 'compress']);
-gulp.task('pub', ['bump', 'compress']);
+gulp.task('pubmajor', function(callback) {
+  runSequence('bump', 'compress', callback);
+});
+gulp.task('pubminor', function(callback) {
+  runSequence('bump', 'compress', callback);
+});
+gulp.task('pub', function(callback) {
+  runSequence('bump', 'compress', callback);
+});
 
 gulp.task('sass', function() {
   gulp.src('./scss/flatland.scss')
@@ -29,18 +36,18 @@ gulp.task('major', function() {
 });
 
 gulp.task('bump', function() {
-  console.log();
+
   var task = gulp.env._[0];
   var type = task === 'pubmajor' ?
     'major' : task === 'pubminor' ?
     'minor' : 'patch';
-  gulp.src('./*.json')
+  return gulp.src('./*.json')
     .pipe(bump({ type: type }))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('compress', function() {
-  gulp.src(['flatland.css', 'devtools.js', 'devtools.html', 'icons/**', '_locales/**', 'manifest.json'])
+  return gulp.src(['flatland.css', 'devtools.js', 'devtools.html', 'icons/**', '_locales/**', 'manifest.json'])
     .pipe(zip('devtools-flatland.zip'))
     .pipe(gulp.dest('./'))
 });
